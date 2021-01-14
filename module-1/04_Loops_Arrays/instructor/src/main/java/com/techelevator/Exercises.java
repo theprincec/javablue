4e302f4fc10f654fbcaee49e0ba2f3e40bc7e8ee
Brian Lauvray <brian@techelevator.com>
2021-01-14 14:01:32 -0500

    Lecture code

--- module-1/04_Loops_Arrays/instructor/src/main/java/com/techelevator/Exercises.java
+++ module-1/04_Loops_Arrays/instructor/src/main/java/com/techelevator/Exercises.java
@@ -72,7 +72,7 @@ public class Exercises {
 	 reverse3([7, 0, 0]) → [0, 0, 7]
 	 */
 	public int[] reverse3(int[] nums) {
-		return new int[] {};
+		return new int[] { nums[2], nums[1], nums[0]};
 	}
 
 	/*
@@ -140,7 +140,18 @@ public class Exercises {
 	 has22([2, 1, 2]) → false
 	 */
 	public boolean has22(int[] nums) {
-		return false;
+		boolean has2Twos = false;
+		
+		for (int i = 0; i < nums.length - 1; i++) {
+			if ( nums[i] == 2) {
+				if (nums[i+1] == 2) {
+					has2Twos = true;
+					break;
+				}
+			}
+		}
+		
+		return has2Twos;
 	}
 	
 	/*
--- module-1/04_Loops_Arrays/lecture-final/src/main/java/com/techelevator/Lecture.java
+++ module-1/04_Loops_Arrays/lecture-final/src/main/java/com/techelevator/Lecture.java
@@ -7,7 +7,7 @@ public class Lecture {
     */
     public int[] returnArray() {
         int[] array = { 80, 8080, 443 };
-        return null;
+        return array;
     }
 
     /*
@@ -15,7 +15,7 @@ public class Lecture {
     */
     public int returnFirstElement() {
         int[] portNumbers = { 80, 8080, 443 };
-        return 1;
+        return portNumbers[0];
     }
 
     /*
@@ -23,21 +23,21 @@ public class Lecture {
     */
     public int returnLastElement() {
         int[] portNumbers = { 80, 8080, 443 };
-        return 1;
+        return portNumbers[2];
     }
 
     /*
     4. Return the first element of the array from the parameters
     */
     public int returnFirstElementOfParam(int[] passedInArray) {
-        return 1;
+        return passedInArray[0];
     }
 
     /*
     5. Return the last element of the array from the parameters
     */
     public int returnLastElementOfParam(int[] passedInArray) {
-        return 1;
+        return passedInArray[ passedInArray.length - 1 ];
     }
 
     /*
@@ -46,13 +46,15 @@ public class Lecture {
     */
     public int returnVariableFromBlock(int number) {
 
+    	 int result;
         { // A new block with scoped variables
 
-            int result = number * 5;
-
+            result = number * 5;
+         
         } // the result variable disappears here
 
-        return number; // We want to return result here. How?
+ 
+        return result; // We want to return result here. How?
     }
 
     /*
