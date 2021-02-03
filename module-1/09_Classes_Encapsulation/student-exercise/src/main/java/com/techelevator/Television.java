d9c7dbd03c38769c4478a3bd108bd580b45f1a30
Christian Cumberlander <ccumberl@kent.edu>
2021-02-03 08:59:39 -0500

    09 HW resubmit

--- module-1/09_Classes_Encapsulation/student-exercise/src/main/java/com/techelevator/Television.java
+++ module-1/09_Classes_Encapsulation/student-exercise/src/main/java/com/techelevator/Television.java
@@ -1,5 +1,86 @@
 package com.techelevator;
 
 public class Television {
+	
+	private boolean isOn;
+	private int currentChannel;
+	private int currentVolume ;
+	
+	
+	
+	
+	
+	
+	
+	
+	public Television() {
+		this.isOn = isOn;
+		this.currentChannel = 3;
+		this.currentVolume = 2;
+	}
+	public boolean isOn() {
+		return isOn;
+	}
+	public int getCurrentChannel() {
+		return currentChannel;
+	}
+	public int getCurrentVolume() {
+		return currentVolume;
+	}
+	
+	
+	public void turnOff() {
+		isOn = false;
+	};
+	
+
+	public void turnOn() {
+		isOn = true;
+	}
+	
+	public void changeChannel(int newChannel) {
+		currentChannel = newChannel;
+	}
+	
+	
+	
+	public void channelUp() {
+		if (currentChannel>17 && isOn) {
+			currentChannel = 18;
+		}else if (isOn) {
+			currentChannel++;
+		}
+		}
+	
+	public void channelDown() {
+		if (currentChannel<4 && isOn) {
+			currentChannel = 18;
+		}else if (isOn) {
+			currentChannel--;
+		}
+		
+	}
+	
+	public void raiseVolume() {
+		
+		
+		if (currentVolume <10 && isOn) {
+			currentVolume++;
+		} else if (isOn) {
+			currentVolume = 10;
+		}
+		
+	}
+	
+	public void lowerVolume() {
+		if (currentVolume >0 && isOn) {
+			currentVolume--;
+		} else if (isOn){
+			currentVolume = 0;
+		}
+		
+	}
+	
+	
 
 }
