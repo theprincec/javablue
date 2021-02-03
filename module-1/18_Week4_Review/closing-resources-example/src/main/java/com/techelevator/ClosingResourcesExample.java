0d9cf994d998cbbe20c62a2610633d2608e07d3b
Brian Lauvray <brian@techelevator.com>
2021-02-03 14:58:56 -0500

    Review Lecture code

--- module-1/18_Week4_Review/closing-resources-example/src/main/java/com/techelevator/ClosingResourcesExample.java
+++ module-1/18_Week4_Review/closing-resources-example/src/main/java/com/techelevator/ClosingResourcesExample.java
@@ -1,18 +1,26 @@
 package com.techelevator;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
 
 public class ClosingResourcesExample {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws IOException {
 		
 		File file = new File("testfile.txt");
 		
-		PrintWriter writer = new PrintWriter(file);
-		writer.write("This text was written from Java");
+		try (PrintWriter writer = new PrintWriter(file);
+				BufferedWriter bufferedWriter = new BufferedWriter(writer)) {
 		
+			writer.write("This text was written from Java with Try with Resource");
+		
+		
+		}
+		
+
 	}
 
 }
