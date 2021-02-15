4d456e509fe001d3f1248c5aeab23a2e5aa5d3d5
Christian Cumberlander <ccumberl@kent.edu>
2021-02-15 08:47:21 -0500

    04 HW submit

---
+++ module-2/04_INSERT_UPDATE_and_DELETE/student-exercise/animal_hospital.sql
@@ -0,0 +1,68 @@
+--create database animal_hospital;
+drop table if exists pet;
+drop table if exists owner;
+drop table if exists visit;
+drop table if exists procedure;
+drop table if exists invoice;
+
+
+
+create table pet(
+        pet_id serial primary key,
+        owner_id int,
+        name varchar(20) not null,
+        type varchar (20),
+        age int not null,
+
+        CONSTRAINT fk_pet_owner_id FOREIGN KEY (owner_id) REFERENCES owner(owner_id),
+        CONSTRAINT Chk_type CHECK (type in ('Dog', 'Cat', 'Bird', 'Turtle', 'Zebra', 'Hippopotamus', 'Salmon', 'Gerbil')
+
+);
+
+
+create table owner(
+        owner_id serial primary key,
+        first_name varchar(20) not null,
+        last_name varchar(20) not null,
+        address varchar(255) not null,
+        city varchar(48) not null,
+        district varchar(48) not null,
+        postal_code varchar(10) not null
+
+);
+
+create table visit(
+        visit_id serial primary key,
+        pet_id int,
+        date DATE,
+        
+        CONSTRAINT fk_visit_pet_id FOREIGN KEY (pet_id) REFERENCES pet(pet_id)
+        
+);
+
+create table procedure(
+        procedure_id serial primary key,
+        type varchar(48)
+
+
+);
+
+create table visit_procedure(
+        procedure_id int,
+        visit_id int,
+        
+        PRIMARY KEY (procedure_id, visit_id),
+        CONSTRAINT fk_procedure_procedure_id FOREIGN KEY (procedure_id) REFERENCES procedure(procedure_id),
+        CONSTRAINT fk_visit_visit_id FOREIGN KEY (visit_id) REFERENCES visit(visit_id)      
+
+);
+
+create table invoice(
+        invoice_id serial primary key,
+        visit_id int,
+        total_cost decimal(7,2),
+        
+        CONSTRAINT fk_invoice_visit_id FOREIGN KEY (visit_id) REFERENCES visit(visit_id)
+
+
+);
--- module-2/04_INSERT_UPDATE_and_DELETE/student-exercise/constraints-and-transactions-exercises.sql
+++ module-2/04_INSERT_UPDATE_and_DELETE/student-exercise/constraints-and-transactions-exercises.sql
@@ -3,24 +3,52 @@
 
 -- 1. Add actors, Hampton Avenue, and Lisa Byway to the actor table.
 
+select * from actor;
+insert into actor (first_name, last_name) values ('Hampton', 'Avenue');
+insert into actor (first_name, last_name) values ('Lisa', 'Byway');
+
 -- 2. Add "Euclidean PI", "The epic story of Euclid as a pizza delivery boy in
 -- ancient Greece", to the film table. The movie was released in 2008 in English.
 -- Since its an epic, the run length is 3hrs and 18mins. There are no special
 -- features, the film speaks for itself, and doesn't need any gimmicks.
+select * from film;
+insert into film (title, description, release_year, language_id, length ) values ('Euclidean PI', 'The epic story of Euclid as a pizza delivery boy in
+-- ancient Greece', '2008', '1', '198');
+
+select * from film
+where title = 'Euclidean PI' ;
+delete from film 
+where film_id = 1003;
 
 -- 3. Hampton Avenue plays Euclid, while Lisa Byway plays his slightly
 -- overprotective mother, in the film, "Euclidean PI". Add them to the film.
+select * from film_actor;
+insert into film_actor values ('201','1001');
+insert into film_actor values ('202','1001');
+
 
 -- 4. Add Mathmagical to the category table.
+select * from category;
+insert into category (category_id, name) values ('17', 'Mathmagical');
+
 
 -- 5. Assign the Mathmagical category to the following films, "Euclidean PI",
 -- "EGG IGBY", "KARATE MOON", "RANDOM GO", and "YOUNG LANGUAGE"
+select * from film;
+--1001, 274, 494, 714, 996
+update film_category
+set category_id = 17
+where film_id in ('1001', '274', '494', '714', '996');
 
 -- 6. Mathmagical films always have a "G" rating, adjust all Mathmagical films
 -- accordingly.
 -- (5 rows affected)
+update film
+set rating = 'G'
+where film_id in ('1001', '274', '494', '714', '996');
 
 -- 7. Add a copy of "Euclidean PI" to all the stores.
+select * from inventory --where film_id = '1001';
 
 -- 8. The Feds have stepped in and have impounded all copies of the pirated film,
 -- "Euclidean PI". The film has been seized from all stores, and needs to be
