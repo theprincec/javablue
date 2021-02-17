bb4caff0f477e6dd4266a75a3a862334d1bc670e
Brian Lauvray <brian@techelevator.com>
2021-02-17 14:58:35 -0500

    Review lecture code

--- module-2/00a-DAO_Review/lecture-final/src/main/java/com/techelevator/CateringSystemCLI.java
+++ module-2/00a-DAO_Review/lecture-final/src/main/java/com/techelevator/CateringSystemCLI.java
@@ -2,24 +2,37 @@ package com.techelevator;
 
 import java.io.FileNotFoundException;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
+
 import com.techelevator.inventory.reader.FileInventoryReader;
 import com.techelevator.inventory.reader.InventoryReader;
+import com.techelevator.inventory.reader.JdbcInventoryReader;
+import com.techelevator.log.JdbcLogDao;
+import com.techelevator.log.LogDao;
 import com.techelevator.view.Menu;
 
 public class CateringSystemCLI {
 
 	private Menu menu;
 	private VendingMachine vendingMachine;
+	private BasicDataSource dataSource;
 	
 	public CateringSystemCLI(Menu menu) {
+		dataSource = new BasicDataSource();
+		dataSource.setUrl("jdbc:postgresql://localhost:5432/vendingmachine");
+		dataSource.setUsername("postgres");
+		dataSource.setPassword("postgres1");
+		
 		this.menu = menu;
 	}
 
 	public void run() {
-		
+		LogDao log = new JdbcLogDao(dataSource);
 		InventoryReader inventoryReader = getInventoryReader();
 		
 		try {
+			// Can pass a DAO in the constructor to be used in the class
 			vendingMachine = new VendingMachine(inventoryReader);
 		} catch (FileNotFoundException e) {
 			menu.showErrorToUser(e.getMessage());
@@ -38,8 +51,11 @@ public class CateringSystemCLI {
 			int choice = menu.getSelectionFromMainMenu();
 			if (choice == 1) {
 				menu.displayItems(vendingMachine.getInventory());
+				// Can pass a DAO as an argument to a method to be used
+				menu.exampleOfUsingLogInAnotherClass(log);
 			} else if (choice == 2) {
-				// Purchase Items
+				int amountToAdd = menu.getAmountToAdd();
+				vendingMachine.addMoney(amountToAdd, log);
 			} else {
 				// Exit
 				System.out.println("Thank you!");
@@ -61,7 +77,9 @@ public class CateringSystemCLI {
 				break;
 			
 			} else if (selection.equalsIgnoreCase("D")) {
-				throw new UnsupportedOperationException("Database not yet implemented!");
+				
+				reader = new JdbcInventoryReader(dataSource);
+				break;
 			
 			} else {
 				menu.showErrorToUser("Invalid selection!");
---
+++ module-2/00a-DAO_Review/lecture-final/src/main/java/com/techelevator/log/JdbcLogDao.java
@@ -0,0 +1,54 @@
+package com.techelevator.log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+
+public class JdbcLogDao implements LogDao {
+
+	private JdbcTemplate jdbcTemplate;
+	
+	public JdbcLogDao(DataSource dataSource) {
+		jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+	
+	@Override
+	public void save(Log log) {
+		String sql = "INSERT INTO log (id, action, beginning_balance, ending_balance, entry_date) VALUES (DEFAULT, ?, ?, ?, ?)";
+		jdbcTemplate.update(sql, log.getAction(), log.getBeginningBalance(), log.getEndingBalance(), log.getEntryDate());
+	}
+
+	@Override
+	public List<Log> getAllLogEntries() {
+		List<Log> entries = new ArrayList<Log>();
+		String sql = "SELECT id, action, beginning_balance, ending_balance, entry_date FROM log";
+		
+		SqlRowSet rows = jdbcTemplate.queryForRowSet(sql);
+		
+		while (rows.next()) {
+			entries.add( mapRowToLog( rows) );
+		}
+		
+		return entries;
+	}
+	
+	private Log mapRowToLog(SqlRowSet row) {
+		Log log = new Log();
+		log.setId( row.getLong("id") );
+		log.setAction( row.getString("action") );
+		log.setBeginningBalance( row.getDouble("beginning_balance"));
+		log.setEndingBalance( row.getDouble("ending_balance"));
+		
+		if (row.getDate("entry_date") != null) {
+			log.setEntryDate( row.getDate("entry_date").toLocalDate() );
+		}
+		
+		return log;
+	}
+	
+
+}
