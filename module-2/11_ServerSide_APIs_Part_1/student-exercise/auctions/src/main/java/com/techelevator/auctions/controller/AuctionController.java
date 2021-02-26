bd0cc8a4de13192db4f92f338252bb36ce4289e4
Christian Cumberlander <ccumberl@kent.edu>
2021-02-26 09:00:43 -0500

    11 hw submit

--- module-2/11_ServerSide_APIs_Part_1/student-exercise/auctions/src/main/java/com/techelevator/auctions/controller/AuctionController.java
+++ module-2/11_ServerSide_APIs_Part_1/student-exercise/auctions/src/main/java/com/techelevator/auctions/controller/AuctionController.java
@@ -3,8 +3,10 @@ package com.techelevator.auctions.controller;
 import com.techelevator.auctions.DAO.AuctionDAO;
 import com.techelevator.auctions.DAO.MemoryAuctionDAO;
 import com.techelevator.auctions.model.Auction;
+
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -17,4 +19,38 @@ public class AuctionController {
         this.dao = new MemoryAuctionDAO();
     }
 
+    
+    @RequestMapping(method=RequestMethod.GET)
+    public List<Auction> list(){
+    	List<Auction> auctions = new ArrayList<>();
+    	auctions = dao.list();
+    	
+    	return auctions;
+    }
+   
+    
+    //not passing
+    @RequestMapping(path="/auctions/{id}", method=RequestMethod.GET)
+    public Auction get(@PathVariable int id) {
+    	return dao.get(id);
+    }
+    
+    
+    @RequestMapping(method=RequestMethod.POST)
+    public Auction create(@RequestBody Auction auction){
+    	return dao.create(auction);
+    }
+    
+    @RequestMapping( method=RequestMethod.GET)
+    public List<Auction> searchByTitle(@RequestParam (required=false) String searchTerm){
+    	List<Auction> auctions = new ArrayList<>();
+    	auctions = dao.searchByTitle(searchTerm);
+    	
+    	return auctions;
+    }
+    
+    
+    
+    
+    
 }
