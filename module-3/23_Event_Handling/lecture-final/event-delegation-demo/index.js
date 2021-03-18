48236f45cb796c36a0cda64318e2ffc9255b8c1f
Brian Lauvray <brian@techelevator.com>
2021-03-18 12:15:59 -0400

    Lecture code

--- module-3/23_Event_Handling/lecture-final/event-delegation-demo/index.js
+++ module-3/23_Event_Handling/lecture-final/event-delegation-demo/index.js
@@ -5,6 +5,12 @@ colors.addEventListener("click", event => {
   }
 });
 
+colors.addEventListener("dblclick", event => {
+	if (event.target.nodeName.toLowerCase() === "li") {
+		event.target.style.backgroundColor = 'transparent';
+	}
+});
+
 function addOrange() {
 	const orange = document.createElement('li');
 	orange.innerText = 'Orange';
@@ -14,4 +20,14 @@ function addOrange() {
 document.addEventListener('DOMContentLoaded', () => {
 	const button = document.getElementById('btnAddOrange');
 	button.addEventListener('click', addOrange);
+
+	document.getElementById('btnAddAnotherColor').addEventListener('click', addColor);
+
 });
+
+function addColor() {
+		const newColor= document.createElement('li');
+		newColor.innerText = document.getElementById('colorpicker').value;
+		colors.appendChild(newColor);
+
+}
