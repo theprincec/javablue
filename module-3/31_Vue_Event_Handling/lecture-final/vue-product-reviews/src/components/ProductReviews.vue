bc6a145df621ad37c840d72bce1b662b30a2461d
Brian Lauvray <brian@techelevator.com>
2021-03-24 13:28:00 -0400

    Added Vue Event Lecture Code

--- module-3/31_Vue_Event_Handling/lecture-final/vue-product-reviews/src/components/ProductReviews.vue
+++ module-3/31_Vue_Event_Handling/lecture-final/vue-product-reviews/src/components/ProductReviews.vue
@@ -1,7 +1,8 @@
 <!-- Template defines the HTML -->
 <template>
-    <div class="main">
+    <div class="main" v-bind:style="'background-color: ' + backgroundColor">
         <h2>Product Reviews for {{ name }}</h2>
+        <!-- v-show hides/shows using css the element based on the boolean condition  -->
         <p class="description" v-show="showDescription">{{ description }}</p>
 
         <select v-model="showDescription">
@@ -12,38 +13,76 @@
 
         <div class="well-display">
             <div class="well">
-                <span class="amount">{{ averageRating }}</span>
+                <span class="amount" v-on:click="filter = 0">{{ averageRating }}</span>
                 Average Rating
             </div>
 
             <div class="well">
-                <span class="amount">{{ numberOfOneStarReviews }}</span>
+                <span class="amount" v-on:click="filter = 1">{{ numberOfOneStarReviews }}</span>
                 1 Star Review{{ numberOfOneStarReviews === 1 ? '' : 's' }}
             </div>
 
             <div class="well">
-                <span class="amount">{{ numberOfTwoStarReviews }}</span>
+                <span class="amount" v-on:click="filter = 2">{{ numberOfTwoStarReviews }}</span>
                 2 Star Review{{ numberOfTwoStarReviews === 1 ? '' : 's' }}
             </div>
 
             <div class="well">
-                <span class="amount">{{ numberOfThreeStarReviews }}</span>
+                <span class="amount" v-on:click="filter = 3">{{ numberOfThreeStarReviews }}</span>
                 3 Star Review{{ numberOfThreeStarReviews === 1 ? '' : 's' }}
             </div>
 
             <div class="well">
-                <span class="amount">{{ numberOfFourStarReviews }}</span>
+                <span class="amount" v-on:click="filter = 4">{{ numberOfFourStarReviews }}</span>
                 4 Star Review{{ numberOfFourStarReviews === 1 ? '' : 's' }}
             </div>
 
             <div class="well">
-                <span class="amount">{{ numberOfFiveStarReviews }}</span>
+                <span class="amount" v-on:click="filter = 5">{{ numberOfFiveStarReviews }}</span>
                 5 Star Review{{ numberOfFiveStarReviews === 1 ? '' : 's' }}
             </div>
         </div>
 
+        <!-- v-show uses CSS to show/hide the element based on the boolean condition -->
+        <div v-show="!showForm">
+            <a href="#" v-on:click.prevent="showForm = true">Add Review</a>
+        </div>
+        <!-- v-on is used to attach an event listener to a dom element -->
+        <!-- v-if adds or removes the element to/from the DOM based on the boolean condition -->
+        <form v-on:submit.prevent="addNewReview()" v-if="showForm">
+            <div class="form-element">
+                <label for="reviewer">Name:</label>
+                <input type="text" id="reviewer" v-model.trim="newReview.reviewer" />
+            </div>
+            <div class="form-element">
+                <label for="title">Title:</label>
+                <input type="text" id="title" v-model.trim="newReview.title" />
+            </div>
+            <div class="form-element">
+                <label for="rating">Rating:</label>
+                <select id="rating" v-model.number="newReview.rating" >
+                    <option value="1">1 Star</option>
+                    <option value="2">2 Stars</option>
+                    <option value="3">3 Stars</option>
+                    <option value="4">4 Stars</option>
+                    <option value="5">5 Stars</option>
+                </select>
+            </div>
+            <div class="form-element">
+                <label for="Review:">Review:</label>
+                <textarea id="review" v-model="newReview.review"></textarea>
+            </div>
+            <input type="submit" value="Save" v-bind:disabled="!isFormValid" />
+            <!-- @ is a shortcut for v-on: 
+                The () on the event handler method are optional as long as it doesn't have arguments -->
+            <input type="button" value="Cancel" @click.prevent="resetForm" />
+        </form>
 
-        <div class="review" v-for="review in reviews" v-bind:key="review.id"
+        <div v-if="filteredReviews.length === 0">
+            <p>No {{ filter }} Star Reviews Found</p>
+        </div>
+
+        <div class="review" v-for="review in filteredReviews" v-bind:key="review.id"
             v-bind:class="{ favorited: review.favorited }">
             <h4>{{ review.reviewer }}</h4>
             <div class="rating">
@@ -55,6 +94,19 @@
             <p>Favorite? <input type="checkbox" v-model="review.favorited" /></p>
         </div>
 
+        <div>
+            Pick a Background Color
+            <ul v-on:click="changeBackgroundColor">  
+                <li>Red</li>
+                <li>Blue</li>
+                <li>Green</li>
+                <li>Yellow</li>
+                <li>Cornflowerblue</li>
+                <li>Goldenrod</li>
+                <li>Transparent</li>
+            </ul>
+
+        </div>
     </div>
 </template>
 
@@ -68,6 +120,10 @@ export default {
             name: 'Cigar Parties for Dummies',
             description: 'Host and plan the perfect cigar party for all your squirrelly friends',
             showDescription: true,
+            showForm: false,
+            filter: 0,
+            newReview: {},
+            backgroundColor: 'transparent',
             reviews: [
                 {
                     reviewer: 'Malcolm Gladwell',
@@ -102,36 +158,53 @@ export default {
         }
     },
     computed: {
+        filteredReviews() {
+            return this.reviews.filter( review => {
+                return this.filter === 0 ? true : this.filter === review.rating;
+            });
+        },
         averageRating() {
             let sum = this.reviews.reduce( (currentSum, review) => {
                 return currentSum + review.rating;
             }, 0);
-            return sum / this.reviews.length;
+            return (sum / this.reviews.length).toFixed(2);
         },
         numberOfOneStarReviews() {
-            return this.reviews.reduce( (currentCount, review) => {
-                return currentCount + (review.rating === 1 ? 1 : 0);
-            }, 0);
+            return this.numberOfReviews(1);
         },
         numberOfTwoStarReviews() {
-            return this.reviews.reduce( (currentCount, review) => {
-                return currentCount + (review.rating === 2 ? 1 : 0);
-            }, 0);
+            return this.numberOfReviews(2);
         },
         numberOfThreeStarReviews() {
-            return this.reviews.reduce( (currentCount, review) => {
-                return currentCount + (review.rating === 3 ? 1 : 0);
-            }, 0);
+            return this.numberOfReviews(3);
         },
         numberOfFourStarReviews() {
-            return this.reviews.reduce( (currentCount, review) => {
-                return currentCount + (review.rating === 4 ? 1 : 0);
-            }, 0);
+            return this.numberOfReviews(4);
         },
         numberOfFiveStarReviews() {
-            return this.reviews.reduce( (currentCount, review) => {
-                return currentCount + (review.rating === 5 ? 1 : 0);
+            return this.numberOfReviews(5);
+        },
+        isFormValid() {
+           return this.newReview.reviewer && this.newReview.title
+                && this.newReview.rating && this.newReview.review; 
+        }
+    },
+    methods: {
+        numberOfReviews(numOfStars) {
+           return this.reviews.reduce( (currentCount, review) => {
+                return currentCount + (review.rating === numOfStars);
             }, 0);
+        },
+        addNewReview() {
+            this.reviews.unshift(this.newReview);
+            this.resetForm();
+        },
+        resetForm() {
+            this.newReview = {};
+            this.showForm = false;
+        },
+        changeBackgroundColor(event) {
+            this.backgroundColor = event.target.innerText;
         }
     }
 }
@@ -194,6 +267,33 @@ div.main div.review h4 {
 
 div.main div.review.favorited {
     background-color: lightyellow;
+}
+
+div.form-element {
+ margin-top: 10px;
+}
+div.form-element > label {
+ display: block;
+}
+div.form-element > input, div.form-element > select {
+ height: 30px;
+ width: 300px;
+}
+div.form-element > textarea {
+ height: 60px;
+ width: 300px;
+}
+form > input[type=button] {
+ width: 100px;
+}
+form > input[type=submit] {
+ width: 100px;
+ margin-right: 10px;
+}
+
+div.main div.well-display div.well span.amount:hover {
+    color: blue;
+    cursor: pointer;
 }
 </style>
 
