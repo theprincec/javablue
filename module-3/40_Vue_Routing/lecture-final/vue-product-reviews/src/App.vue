5797f56c23b7866b9685a45f0028a5ebd2ddbe7b
Brian Lauvray <lauvray.1@osu.edu>
2021-03-29 16:06:50 -0400

    Added Vue Router Lecture and Web Services Tutorials

---
+++ module-3/40_Vue_Routing/lecture-final/vue-product-reviews/src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="app" class="main">
+    <h2>Product Reviews for {{ $store.state.name }}</h2>
+    <p class="description">{{ $store.state.description }}</p>
+
+    <div class="well-display">
+      <average-summary />
+      <star-summary rating="1" />
+      <star-summary rating="2" />
+      <star-summary rating="3" />
+      <star-summary rating="4" />
+      <star-summary rating="5" />
+    </div>
+
+    <add-review />
+    <review-list />
+  </div>
+</template>
+
+<script>
+import AverageSummary from './components/AverageSummary.vue'
+import StarSummary from './components/StarSummary.vue'
+import ReviewList from './components/ReviewList.vue'
+import AddReview from './components/AddReview.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AverageSummary,
+    StarSummary,
+    ReviewList,
+    AddReview
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+div.main {
+    margin: 1rem 0;
+}
+div.main div.well-display {
+    display: flex;
+    justify-content: space-around;
+}
+
+div.main div.well-display div.well {
+    display: inline-block;
+    width: 15%;
+    border: 1px black solid;
+    border-radius: 6px;
+    text-align: center;
+    margin: 0.25rem;
+}
+
+div.main div.well-display div.well span.amount {
+    color: darkslategray;
+    display: block;
+    font-size: 2.5rem;
+}
+
+div.main div.well-display div.well span.amount:hover {
+    color: blue;
+    cursor: pointer;
+}
+</style>
---
+++ module-3/40_Vue_Routing/lecture-final/vue-product-reviews/src/components/ProductReviews.vue
@@ -0,0 +1,134 @@
+<template>
+
+    <div class="main">
+        
+    
+ 
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'product-reviews',
+    data() {
+        return {
+            
+            // showForm and newReviw move to AddReview.vue
+            
+        
+        }
+    },
+    computed: {
+        // filteredReviews: move to ReviewList.vue
+     
+
+
+        // isFormValid: Move to AddReview.vue
+  
+    },
+    methods: {
+  
+        // addNewReview and resetForm: Move to AddReview.vue
+
+    }
+}
+</script>
+
+
+<style scoped>
+
+/* Move these styles to App.vue */
+div.main {
+    margin: 1rem 0;
+}
+div.main div.well-display {
+    display: flex;
+    justify-content: space-around;
+}
+
+div.main div.well-display div.well {
+    display: inline-block;
+    width: 15%;
+    border: 1px black solid;
+    border-radius: 6px;
+    text-align: center;
+    margin: 0.25rem;
+}
+
+div.main div.well-display div.well span.amount {
+    color: darkslategray;
+    display: block;
+    font-size: 2.5rem;
+}
+
+div.main div.well-display div.well span.amount:hover {
+    color: blue;
+    cursor: pointer;
+}
+
+
+/* Move these styles to ReviewDisplay.vue */
+div.main div.review {
+    border: 1px black solid;
+    border-radius: 6px;
+    padding: 1rem;
+    margin: 10px;
+}
+
+div.main div.review div.rating {
+    height: 2rem;
+    display: inline-block;
+    vertical-align: top;
+    margin: 0 0.5rem;
+}
+
+div.main div.review div.rating img {
+    height: 100%;
+}
+
+div.main div.review p {
+    margin: 20px;
+}
+
+div.main div.review h3 {
+    display: inline-block;
+}
+
+div.main div.review h4 {
+    font-size: 1rem;
+}
+
+div.main div.review.favorited {
+    background-color: lightyellow;
+}
+
+
+
+/* Move these styles to AddReview.vue */
+div.form-element {
+ margin-top: 10px;
+}
+div.form-element > label {
+ display: block;
+}
+div.form-element > input, div.form-element > select {
+ height: 30px;
+ width: 300px;
+}
+div.form-element > textarea {
+ height: 60px;
+ width: 300px;
+}
+form > input[type=button] {
+ width: 100px;
+}
+form > input[type=submit] {
+ width: 100px;
+ margin-right: 10px;
+}
+
+
+</style>
+
+
